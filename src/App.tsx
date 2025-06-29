69ee1823c42523b84a6de2c3427b65e425827cb2
RoopaSowmya14 <kandularoopasowmya@gmail.com>
2025-06-30 01:20:47 +0530

    removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./Sections/Home/Home";
 import About from "./Sections/About/About";
-import Projects from "./Sections/Projects/Projects";
 import Contact from "./Sections/Contact/Contact";
 import "./styles/global.css";
 import Navbar from "./Sections/NavBar/NavBar";
